418d208b76c17d6c1e624e07b2a1f6b63cc64212
sekterra <sekterra@nate.com>
2019-05-10 18:08:43 +0900

    초기설정

---
+++ src/views/TableList.vue
@@ -0,0 +1,168 @@
+<template>
+  <v-container
+    fill-height
+    fluid
+    grid-list-xl
+  >
+    <v-layout
+      justify-center
+      wrap
+    >
+      <v-flex
+        md12
+      >
+        <material-card
+          color="green"
+          title="Simple Table1"
+          text="Here is a subtitle for this table"
+        >
+          <v-data-table
+            :headers="headers"
+            :items="items"
+            hide-actions
+          >
+            <template
+              slot="headerCell"
+              slot-scope="{ header }"
+            >
+              <span
+                class="subheading font-weight-light text-success text--darken-3"
+                v-text="header.text"
+              />
+            </template>
+            <template
+              slot="items"
+              slot-scope="{ item }"
+            >
+              <td>{{ item.name }}</td>
+              <td>{{ item.country }}</td>
+              <td>{{ item.city }}</td>
+              <td class="text-xs-right">{{ item.salary }}</td>
+            </template>
+          </v-data-table>
+        </material-card>
+      </v-flex>
+      <v-flex
+        md12
+      >
+        <material-card
+          color="green"
+          flat
+          full-width
+          title="Table on Plain Background"
+          text="Here is a subtitle for this table"
+        >
+          <v-data-table
+            :headers="headers"
+            :items="items.slice(0, 7)"
+            hide-actions
+          >
+            <template
+              slot="headerCell"
+              slot-scope="{ header }"
+            >
+              <span
+                class="subheading font-weight-light text--darken-3"
+                v-text="header.text"
+              />
+            </template>
+            <template
+              slot="items"
+              slot-scope="{ item }"
+            >
+              <td>{{ item.name }}</td>
+              <td>{{ item.country }}</td>
+              <td>{{ item.city }}</td>
+              <td class="text-xs-right">{{ item.salary }}</td>
+            </template>
+          </v-data-table>
+        </material-card>
+      </v-flex>
+      <v-flex md12>
+        <hot-table
+          :data="data"
+          row-headers="true"
+          col-headers="true"
+          license-key="non-commercial-and-evaluation"
+        />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import { HotTable } from '@handsontable/vue'
+
+export default {
+  components: {
+    HotTable
+  },
+  data: () => ({
+    headers: [
+      {
+        sortable: false,
+        text: 'Name',
+        value: 'name'
+      },
+      {
+        sortable: false,
+        text: 'Country',
+        value: 'country'
+      },
+      {
+        sortable: false,
+        text: 'City',
+        value: 'city'
+      },
+      {
+        sortable: false,
+        text: 'Salary',
+        value: 'salary',
+        align: 'right'
+      }
+    ],
+    items: [
+      {
+        name: 'Dakota Rice',
+        country: 'Niger',
+        city: 'Oud-Tunrhout',
+        salary: '$35,738'
+      },
+      {
+        name: 'Minerva Hooper',
+        country: 'Curaçao',
+        city: 'Sinaai-Waas',
+        salary: '$23,738'
+      }, {
+        name: 'Sage Rodriguez',
+        country: 'Netherlands',
+        city: 'Overland Park',
+        salary: '$56,142'
+      }, {
+        name: 'Philip Chanley',
+        country: 'Korea, South',
+        city: 'Gloucester',
+        salary: '$38,735'
+      }, {
+        name: 'Doris Greene',
+        country: 'Malawi',
+        city: 'Feldkirchen in Kārnten',
+        salary: '$63,542'
+      }, {
+        name: 'Mason Porter',
+        country: 'Chile',
+        city: 'Gloucester',
+        salary: '$78,615'
+      }
+    ],
+    data: [
+      ['', 'Ford', 'Volvo', 'Toyota', 'Honda'],
+      ['2016', 10, 11, 12, 13],
+      ['2017', 20, 11, 14, 13],
+      ['2018', 30, 15, 12, 13]
+    ]
+  })
+}
+</script>
+
+<style src="../../node_modules/handsontable/dist/handsontable.full.css"></style>
